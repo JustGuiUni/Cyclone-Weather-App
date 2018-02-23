9732281e1419ae1e70fabccf5b244af6df4c854a
DanyalGolabi <dgnt@hotmail.co.uk>
2018-02-23 18:39:08 +0000

    Updated Bottom Nav
    Updated so that the entire button is clickable rather than just the icon.

--- src/components/bottomnav/bottomnav_index.js
+++ src/components/bottomnav/bottomnav_index.js
@@ -7,8 +7,8 @@ export default class Bottomnav extends Component {
    render() {
       return (
         <nav class={ style_bottomnav.container }>
-        <bottom-button><Link activeClassName="active" href="/index">	<i class="fa fa-home"></i></Link></bottom-button>
-        <bottom-button><Link activeClassName="active" href="/routes"> <i class="fa fa-clock-o"></i></Link></bottom-button>
+        <Link activeClassName="active" href="/index"><bottom-button><i class="fa fa-home"></i></bottom-button></Link>
+        <Link activeClassName="active" href="/routes"><bottom-button><i class="fa fa-clock-o"></i></bottom-button></Link>
         <bottom-button><i class="fa fa-map-marker"></i></bottom-button>
         <bottom-button><i class="fa fa-bullseye"></i></bottom-button>
         </nav>
