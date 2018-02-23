f5034844bc4b4af5d73bc02099a74e9163650ff5
aajfranklin <aajfranklin@gmail.com>
2018-02-23 22:18:37 +0000

    Styles cascade from iPhone, corrects button for route page, cleans navbar

--- src/components/bottomnav/bottomnav_index.js
+++ src/components/bottomnav/bottomnav_index.js
@@ -8,8 +8,8 @@ export default class Bottomnav extends Component {
       return (
         <nav class={ style_bottomnav.container }>
         <Link activeClassName="active" href="/index"><bottom-button><i class="fa fa-home"></i></bottom-button></Link>
-        <Link activeClassName="active" href="/routes"><bottom-button><i class="fa fa-clock-o"></i></bottom-button></Link>
-        <bottom-button><i class="fa fa-map-marker"></i></bottom-button>
+        <bottom-button><i class="fa fa-clock-o"></i></bottom-button>
+        <Link activeClassName="active" href="/routes"><bottom-button><i class="fa fa-map-marker"></i></bottom-button></Link>
         <bottom-button><i class="fa fa-bullseye"></i></bottom-button>
         </nav>
       );
--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -2,17 +2,7 @@
 import { h, render, Component } from 'preact';
 // import stylesheets for iphone, button
 import style from './style.less';
-import style_home from '../home/style_home'
 import style_topnav from '../topnav/style_topnav';
-import style_bottomnav from '../bottomnav/style_bottomnav';
-
-// import jquery for API calls
-import $ from 'jquery';
-// import the Button component
-import Button from '../button';
-//Import pages
-//import Home from '../index.js';
-//import Route from './index.js';
 
 export default class Route extends Component {
 
@@ -27,21 +17,20 @@ export default class Route extends Component {
 
     // dsiplay route mapping
 		return (
-    		<div class={ style_home.container }>
+    		<div class={ style.container }>
+
+		      	<div class={style_topnav.container}>
+		        	<routesearch><input type="text" placeholder="Search my routes..." id="searchField"></input></routesearch>
+		        	<buttonleft><a><i class="fa fa-search" id="searchButton"></i></a></buttonleft>
+		        	<buttonright><a><i class="fa fa-cog"></i></a></buttonright>
+		      	</div>
+
+		      	<div class={style.plusbutton}>
+					<button-left><i class="fa fa-plus-circle" id="addRoute"> <text-add>Add Route</text-add></i></button-left><br />
+	    	  	</div>
 
 
-	      	<div class={style_topnav.containerRoute}>
-	        	<search-bar><input type="text" placeholder="Search my routes..." id="searchField"></input></search-bar>
-	        	<button-left><a><i class="fa fa-search" id="searchButton"></i></a></button-left>
-	        	<button-right><a><i class="fa fa-cog"></i></a></button-right>
-	      	</div>
-
-	      	<div class={style.plusbutton}>
-				<button-left><i class="fa fa-plus-circle" id="addRoute"> <text-add>Add Route</text-add></i></button-left><br />
-    	  	</div>
-
-
-			<div class={ style.grid }>
+				<div class={ style.grid }>
 					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>WORK</td><td>Start: SE27</td></tr><tr><td>6 pm</td><td>End: N19</td></tr></table></div>
 					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit" id="editroute"></i></button-right></div>
 				</div>
@@ -59,10 +48,7 @@ export default class Route extends Component {
 				<div class={ style.grid }>
 					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>GYM</td><td>Start: W12</td></tr><tr><td>6 pm</td><td>End: W18</td></tr></table></div>
 					<div class= {style.gridchild2 }> <button-right><i class="fa fa-edit"></i></button-right></div>
-          </div>
-
-
-
+          		</div>
 			</div>
 		);
 	}
