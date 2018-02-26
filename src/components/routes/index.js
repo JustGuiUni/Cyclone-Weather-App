daa11fe5410021578627e0747f44c16c8dc6d848
aajfranklin <aajfranklin@gmail.com>
2018-02-26 21:43:59 +0000

    Updates Alex's bad CSS to standard format

--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -2,7 +2,8 @@
 import { h, render, Component } from 'preact';
 // import stylesheets for iphone, button
 import style from './style.less';
-import style_topnav from '../topnav/style_topnav';
+import style_topnav from '../topnav/style';
+import style_iphone from '../iphone/style';
 
 export default class Route extends Component {
 
@@ -19,16 +20,28 @@ export default class Route extends Component {
 		return (
     		<div class={ style.container }>
 
-		      	<div class={style_topnav.container}>
-		        	<routesearch><input type="text" placeholder="Search my routes..." id="searchField"></input></routesearch>
-		        	<buttonleft><a><i class="fa fa-search" id="searchButton"></i></a></buttonleft>
-		        	<buttonright><a><i class="fa fa-cog"></i></a></buttonright>
+		      	<div class={ style_topnav.container }>
+		        	<input class={ style_topnav.input } type="route" placeholder="Search my routes..." id="searchField"></input>
+		        	<div class={ style_topnav.buttonleft }><a><i class="fa fa-search" id="searchButton"></i></a></div>
+		        	<div class={ style_topnav.buttonright }><a><i class="fa fa-cog"></i></a></div>
 		      	</div>
 
+<<<<<<< Updated upstream
 		      	<div class={style.plusbutton}>
 					<button-left><i class="fa fa-plus-circle" id="addRoute"> <text-add>Add Route</text-add></i></button-left><br />
 	    	  	</div>
 
+=======
+                <div class={ style.plusbutton }>
+                    <button-left><i class="fa fa-plus-circle" id="addRoute"> <text-add>Add Route</text-add></i></button-left><br />
+	    	  	</div>
+
+                <div class={ style_iphone.homepage }>
+    				<div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>UNI</td><td>Start: Home</td></tr><tr><td>8 am</td><td>End: QMUL</td></tr></table></div>
+    					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit" id="editroute"></i></button-right></div>
+    				</div>
+>>>>>>> Stashed changes
 
 				<div class={ style.grid }>
 					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>WORK</td><td>Start: SE27</td></tr><tr><td>6 pm</td><td>End: N19</td></tr></table></div>
@@ -40,6 +53,7 @@ export default class Route extends Component {
 					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit" id="editroute"></i></button-right></div>
 				</div>
 
+<<<<<<< Updated upstream
 				<div class={ style.grid }>
 					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>UNI</td><td>Start: NE2</td></tr><tr><td>6 pm</td><td>End: SW1</td></tr></table></div>
 					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit"></i></button-right></div>
@@ -49,6 +63,23 @@ export default class Route extends Component {
 					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>GYM</td><td>Start: W12</td></tr><tr><td>6 pm</td><td>End: W18</td></tr></table></div>
 					<div class= {style.gridchild2 }> <button-right><i class="fa fa-edit"></i></button-right></div>
           		</div>
+=======
+    				<div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>COFFEE TIME</td><td>Start: Home</td></tr><tr><td>11 am</td><td>End: Coffee store</td></tr></table></div>
+    					<div class= {style.gridchild2 }> <button-right><i class="fa fa-edit"></i></button-right></div>
+                    </div>
+
+                    <div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>FAMILY</td><td>Start: Home</td></tr><tr><td>5 pm</td><td>End: Family place</td></tr></table></div>
+    					<div class= {style.gridchild2 }> <button-right><i class="fa fa-edit"></i></button-right></div>
+                    </div>
+
+                    <div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>JAMES</td><td>Start: Home</td></tr><tr><td>12 am</td><td>End: James place</td></tr></table></div>
+    					<div class= {style.gridchild2 }> <button-right><i class="fa fa-edit"></i></button-right></div>
+                    </div>
+                </div>
+>>>>>>> Stashed changes
 			</div>
 		);
 	}
