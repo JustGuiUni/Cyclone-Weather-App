93b45587107fc10ecda101cc0cfbfe0eeaf31425
KimJRem <k.j.k.remmers@se17.qmul.ac.uk>
2018-02-26 15:45:05 +0000

    Style changes routes page
    I made the changes to the routes pages. Only made changes to the routes index.js and routes style.less files

--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -25,30 +25,42 @@ export default class Route extends Component {
 		        	<buttonright><a><i class="fa fa-cog"></i></a></buttonright>
 		      	</div>
 
-		      	<div class={style.plusbutton}>
-					<button-left><i class="fa fa-plus-circle" id="addRoute"> <text-add>Add Route</text-add></i></button-left><br />
+            <div class={style.plusbutton}>
+					    <button-left><i class="fa fa-plus-circle" id="addRoute"> <text-add>Add Route</text-add></i></button-left><br />
 	    	  	</div>
 
+            <homepage>
+    				<div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>UNI</td><td>Start: Home</td></tr><tr><td>8 am</td><td>End: QMUL</td></tr></table></div>
+    					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit" id="editroute"></i></button-right></div>
+    				</div>
 
-				<div class={ style.grid }>
-					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>WORK</td><td>Start: SE27</td></tr><tr><td>6 pm</td><td>End: N19</td></tr></table></div>
-					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit" id="editroute"></i></button-right></div>
-				</div>
+    				<div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>GYM</td><td>Start: QMUL</td></tr><tr><td>5 pm</td><td>End: Gym</td></tr></table></div>
+    					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit" id="editroute"></i></button-right></div>
+    				</div>
 
-				<div class={ style.grid }>
-					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>HOME</td><td>Start: E16</td></tr><tr><td>6 pm</td><td>End: SE18</td></tr></table></div>
-					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit" id="editroute"></i></button-right></div>
-				</div>
+    				<div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>HOME</td><td>Start: Gym</td></tr><tr><td>8 pm</td><td>End: Home</td></tr></table></div>
+    					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit"></i></button-right></div>
+    				</div>
 
-				<div class={ style.grid }>
-					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>UNI</td><td>Start: NE2</td></tr><tr><td>6 pm</td><td>End: SW1</td></tr></table></div>
-					<div class= {style.gridchild2 }><button-right><i class="fa fa-edit"></i></button-right></div>
-				</div>
+    				<div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>COFFEE TIME</td><td>Start: Home</td></tr><tr><td>11 am</td><td>End: Coffee store</td></tr></table></div>
+    					<div class= {style.gridchild2 }> <button-right><i class="fa fa-edit"></i></button-right></div>
+            </div>
 
-				<div class={ style.grid }>
-					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>GYM</td><td>Start: W12</td></tr><tr><td>6 pm</td><td>End: W18</td></tr></table></div>
-					<div class= {style.gridchild2 }> <button-right><i class="fa fa-edit"></i></button-right></div>
-          		</div>
+            <div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>FAMILY</td><td>Start: Home</td></tr><tr><td>5 pm</td><td>End: Family place</td></tr></table></div>
+    					<div class= {style.gridchild2 }> <button-right><i class="fa fa-edit"></i></button-right></div>
+            </div>
+
+            <div class={ style.grid }>
+    					<div class= {style.gridchild1 }><table class={ style.table }><tr><td>JAMES</td><td>Start: Home</td></tr><tr><td>12 am</td><td>End: James place</td></tr></table></div>
+    					<div class= {style.gridchild2 }> <button-right><i class="fa fa-edit"></i></button-right></div>
+            </div>
+
+            </homepage>
 			</div>
 		);
 	}
