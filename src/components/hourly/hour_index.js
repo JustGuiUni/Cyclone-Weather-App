abdc15f238829a29a7f88a76473aa31080738fd6
aajfranklin <aajfranklin@gmail.com>
2018-03-07 16:40:33 +0000

    Consolidates common functions into helpers.js

--- src/components/hourly/hour_index.js
+++ src/components/hourly/hour_index.js
@@ -1,9 +1,12 @@
 // import preact
 import { h, render, Component } from 'preact';
-// import stylesheets for iphone, button
 
+// import stylesheets for iphone, button
 import style from '../iphone/style.less';
 import style_hourly from './style.less';
+
+// import helper functions for API response parsing
+import {ParseConditions, ParseWind} from '../../helpers.js';
 
 export default class Hourly extends Component {
 
@@ -56,10 +59,10 @@ export default class Hourly extends Component {
 		}
 
 		// call two additional helper functions to parse numeric values for wind direction and conditions into English
-		var cond_icon = this.parseConditions(conditions, sunstate);
+		var cond_icon = ParseConditions(conditions, sunstate);
 		conditions = cond_icon[0];
 		this.state.icon = cond_icon[1];
-		wind_direction = this.parseWind(wind_direction);
+		wind_direction = ParseWind(wind_direction);
 
 		// set states for fields so they can be rendered later on
 		this.state.temp = temp_c + "°";
@@ -94,10 +97,10 @@ export default class Hourly extends Component {
 		var sunstate = forecast['sys']['pod'];
 
 		// call two additional helper functions to parse numeric values for wind direction and conditions into English
-		var cond_icon = this.parseConditions(conditions, sunstate);
+		var cond_icon = ParseConditions(conditions, sunstate);
 		conditions = cond_icon[0];
 		var icon = cond_icon[1];
-		wind_direction = this.parseWind(wind_direction);
+		wind_direction = ParseWind(wind_direction);
 
 		this.state.temp = temp_c + "°";
 		this.state.cond = conditions;
@@ -110,124 +113,6 @@ export default class Hourly extends Component {
 		}
 
 		this.state.fetched = true;
-	}
-
-	// Open Weather Map uses an id code to indicate weather conditions
-	// This function converts an id code into an English language description and sets the appropriate icon
-	parseConditions = (c, sunstate) => {
-
-		var i = "";
-
-		if (200 <= c && c <= 232){
-			c = "Thunderstorm";
-			if (sunstate == "d") {
-				i = "wi wi-day-thunderstorm";
-			} else {
-				i = "wi wi-night-alt-thunderstorm";
-			}
-		} else if (300 <= c && c <= 321){
-			c = "Drizzle";
-			if (sunstate == "d") {
-				i = "wi wi-day-showers";
-			} else {
-				i = "wi wi-night-alt-showers";
-			}
-		} else if (c == 500 || c == 520){
-			c = "Light Rain";
-			if (sunstate == "d") {
-				i = "wi wi-day-showers";
-			} else {
-				i = "wi wi-night-alt-showers";
-			}
-		} else if (c == 501 || c == 521){
-			c = "Moderate Rain";
-			if (sunstate == "d") {
-				i = "wi wi-day-showers";
-			} else {
-				i = "wi wi-night-alt-showers";
-			}
-		} else if ((502 <= c && c <= 504) || (522 <= c && c <= 531)){
-			c = "Heavy Rain";
-			if (sunstate == "d") {
-				i = "wi wi-day-rain";
-			} else {
-				i = "wi wi-night-alt-rain";
-			}
-		} else if (c == 511){
-			c = "Freezing Rain";
-			if (sunstate == "d") {
-				i = "wi wi-day-sleet";
-			} else {
-				i = "wi wi-night-alt-sleet";
-			}
-		} else if (600 <= c && c <= 622){
-			c = "Snow";
-			if (sunstate == "d") {
-				i = "wi wi-day-snow";
-			} else {
-				i = "wi wi-night-alt-snow";
-			}
-		} else if (701 == c) {
-			c = "Mist";
-			if (sunstate == "d") {
-				i = "wi wi-day-fog";
-			} else {
-				i = "wi wi-night-fog";
-			}
-		} else if (721 == c) {
-			c = "Haze";
-			if (sunstate == "d") {
-				i = "wi wi-day-haze";
-			} else {
-				i = "wi wi-night-fog";
-			}
-		} else if (741 == c) {
-			c = "Fog";
-			if (sunstate == "d") {
-				i = "wi wi-day-fog";
-			} else {
-				i = "wi wi-night-fog";
-			}
-		} else if (800 == c) {
-			c = "Clear Sky"
-			if (sunstate == "d") {
-				i = "wi wi-day-sunny";
-			} else {
-				i = "wi wi-night-clear";
-			}
-		} else {
-			c = "Clouds";
-			if (sunstate == "d") {
-				i = "wi wi-day-cloudy";
-			} else {
-				i = "wi wi-night-alt-cloudy";
-			}
-		}
-		return [c,i];
-	}
-
-	// Open Weather Map uses degrees from North for wind direction
-	// This function converts degress from North into an 8-point-compass direction
-	parseWind = (w) => {
-		if (22.5 < w && w <= 67.5) {
-			w = "NE";
-		} else if (67.5 < w && w <= 112.5) {
-			w = "E";
-		} else if (112.5 < w && w <= 157.5) {
-			w = "SE";
-		} else if (157.5 < w && w <= 202.5) {
-			w = "S";
-		} else if (202.5 < w && w <= 247.5) {
-			w = "SW";
-		} else if (247.5 < w && w <= 292.5) {
-			w = "W";
-		} else if (292.5 < w && w <= 337.5) {
-			w = "NW";
-		} else {
-			w = "N";
-		}
-
-		return w;
 	}
 
 	render() {
--- src/components/routeforecast/index.js
+++ src/components/routeforecast/index.js
@@ -1,11 +1,11 @@
 // import preact
 import { h, render, Component } from 'preact';
 
-// import jquery for API calls
-import $ from 'jquery';
-
 // import stylesheet for iphone
 import style from '../iphone/style';
+
+// import helper functions for API response parsing
+import {FetchWeatherForecast, ParseConditions, ParseWind} from '../../helpers.js';
 
 export default class Routeforecast extends Component {
 
@@ -29,7 +29,6 @@ export default class Routeforecast extends Component {
 	// set initial values for the route
 	constructor(props){
 		super(props);
-		this.state.temp1 = "";
 		this.state.count = 0;
 		this.expand = this.expand.bind(this);
 		this.state.detailsid = this.props.num + "d";
@@ -38,27 +37,14 @@ export default class Routeforecast extends Component {
 
 	// a call to fetch weather forecasts for each end of the route via open weather map
 	fetchForecastData = () => {
-
-		var url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + this.props.lat1 + "&lon=" + this.props.lon1 + "&appid=d237a7f64a603e590c18e8f7479ed65c";
-		$.ajax({
-			url: url,
-			dataType: "json",
-			async: false,
-			success : this.parseForecastResponse,
-			error : function(req, err){ console.log('API call failed ' + err); }
-		})
-		console.log("Call Made - start of route")
+		var parsed_json = FetchWeatherForecast(this.props.lat1, this.props.lon1);
+		console.log("Call Made - Start of Route")
+		this.parseForecastResponse(parsed_json);
 		this.state.count = 1;
 
-		url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + this.props.lat2 + "&lon=" + this.props.lon2 + "&appid=d237a7f64a603e590c18e8f7479ed65c";
-		$.ajax({
-			url: url,
-			dataType: "json",
-			async: false,
-			success : this.parseForecastResponse,
-			error : function(req, err){ console.log('API call failed ' + err); }
-		})
-		console.log("Call Made - end of route")
+		parsed_json = FetchWeatherForecast(this.props.lat2, this.props.lon2);
+		console.log("Call Made - End of Route")
+		this.parseForecastResponse(parsed_json);
 		this.state.count =0;
 	}
 
@@ -87,10 +73,10 @@ export default class Routeforecast extends Component {
 		var sunstate = forecast['sys']['pod'];
 
 		// call two additional helper functions to parse numeric values for wind direction and conditions into English
-		var cond_icon = this.parseConditions(conditions, sunstate);
+		var cond_icon = ParseConditions(conditions, sunstate);
 		conditions = cond_icon[0];
 		var icon = cond_icon[1];
-		wind_direction = this.parseWind(wind_direction);
+		wind_direction = ParseWind(wind_direction);
 
 		// set states for fields so they can be rendered later on
 		if (this.state.count == 0) {
@@ -106,125 +92,6 @@ export default class Routeforecast extends Component {
 			this.state.windd2 = wind_direction;
 			this.state.icon2 = icon;
 		}
-	}
-
-
-	// Open Weather Map uses an id code to indicate weather conditions
-	// This function converts an id code into an English language description and sets the appropriate icon
-	parseConditions = (c, sunstate) => {
-
-		var i = "";
-
-		if (200 <= c && c <= 232){
-			c = "Thunderstorm";
-			if (sunstate == "d") {
-				i = "wi wi-day-thunderstorm";
-			} else {
-				i = "wi wi-night-alt-thunderstorm";
-			}
-		} else if (300 <= c && c <= 321){
-			c = "Drizzle";
-			if (sunstate == "d") {
-				i = "wi wi-day-showers";
-			} else {
-				i = "wi wi-night-alt-showers";
-			}
-		} else if (c == 500 || c == 520){
-			c = "Light Rain";
-			if (sunstate == "d") {
-				i = "wi wi-day-showers";
-			} else {
-				i = "wi wi-night-alt-showers";
-			}
-		} else if (c == 501 || c == 521){
-			c = "Moderate Rain";
-			if (sunstate == "d") {
-				i = "wi wi-day-showers";
-			} else {
-				i = "wi wi-night-alt-showers";
-			}
-		} else if ((502 <= c && c <= 504) || (522 <= c && c <= 531)){
-			c = "Heavy Rain";
-			if (sunstate == "d") {
-				i = "wi wi-day-rain";
-			} else {
-				i = "wi wi-night-alt-rain";
-			}
-		} else if (c == 511){
-			c = "Freezing Rain";
-			if (sunstate == "d") {
-				i = "wi wi-day-sleet";
-			} else {
-				i = "wi wi-night-alt-sleet";
-			}
-		} else if (600 <= c && c <= 622){
-			c = "Snow";
-			if (sunstate == "d") {
-				i = "wi wi-day-snow";
-			} else {
-				i = "wi wi-night-alt-snow";
-			}
-		} else if (701 == c) {
-			c = "Mist";
-			if (sunstate == "d") {
-				i = "wi wi-day-fog";
-			} else {
-				i = "wi wi-night-fog";
-			}
-		} else if (721 == c) {
-			c = "Haze";
-			if (sunstate == "d") {
-				i = "wi wi-day-haze";
-			} else {
-				i = "wi wi-night-fog";
-			}
-		} else if (741 == c) {
-			c = "Fog";
-			if (sunstate == "d") {
-				i = "wi wi-day-fog";
-			} else {
-				i = "wi wi-night-fog";
-			}
-		} else if (800 == c) {
-			c = "Clear Sky"
-			if (sunstate == "d") {
-				i = "wi wi-day-sunny";
-			} else {
-				i = "wi wi-night-clear";
-			}
-		} else {
-			c = "Clouds";
-			if (sunstate == "d") {
-				i = "wi wi-day-cloudy";
-			} else {
-				i = "wi wi-night-alt-cloudy";
-			}
-		}
-		return [c,i];
-	}
-
-	// Open Weather Map uses degrees from North for wind direction
-	// This function converts degress from North into an 8-point-compass direction
-	parseWind = (w) => {
-		if (22.5 < w && w <= 67.5) {
-			w = "NE";
-		} else if (67.5 < w && w <= 112.5) {
-			w = "E";
-		} else if (112.5 < w && w <= 157.5) {
-			w = "SE";
-		} else if (157.5 < w && w <= 202.5) {
-			w = "S";
-		} else if (202.5 < w && w <= 247.5) {
-			w = "SW";
-		} else if (247.5 < w && w <= 292.5) {
-			w = "W";
-		} else if (292.5 < w && w <= 337.5) {
-			w = "NW";
-		} else {
-			w = "N";
-		}
-
-		return w;
 	}
 
 	// sets the routedetails element to open when the dropdown button is clicked
